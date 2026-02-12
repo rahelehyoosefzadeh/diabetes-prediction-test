0c54e53bb436b3ae11a3e32ad26919fe484dd972
raheleh.yoosefzadeh <raheleh.yoosefzadeh@avanser.com.au>
2026-02-12 21:13:16 +0330

    feature: train phase

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import pandas as pd
 data = pd.read_csv('diabetes.csv')
 print(data.head())
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+X = data.drop('Outcome', axis=1)
+y = data['Outcome']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
